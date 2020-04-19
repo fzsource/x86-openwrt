05385875827534d316e2a72f844602f6b57cb1c5
fzsource <63889910+fzsource@users.noreply.github.com>
2020-04-19 17:47:53 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.100.240/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.18.254/g' package/base-files/files/bin/config_generate
 #src-git lienol https://github.com/Lienol/openwrt-package
 git clone https://github.com/vernesong/OpenClash                                 package/clash
 git clone https://github.com/rufengsuixing/luci-app-adguardhome                  package/adguardhome
